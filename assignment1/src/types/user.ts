8bc504fa71be875d5c9eaed56cb024413d445a91
SEBASTIAN DE WOLF <s3922403@student.rmit.edu.au>
2025-04-12 17:11:16 +1000

    started on creating user

---
+++ assignment1/src/types/user.ts
@@ -0,0 +1,14 @@
+export type User = {
+    id: string;
+    email: string;
+    password: string;
+  };
+  
+  export const DEFAULT_USERS: User[] = [
+    { id: "1", email: "meow", password: "password123" },
+    { id: "2", email: "loverboy", password: "password456" },
+  ];
+
+  //1 = Tutor!! 
+  //2 = Lecturer!!
+  
