03f5ef4b783b53ba36a0e3353dadb218bd7d3021
SEBASTIAN DE WOLF <s3922403@student.rmit.edu.au>
2025-04-12 18:14:23 +1000

    deleted silly line oops

--- assignment1/src/pages/_app.tsx
+++ assignment1/src/pages/_app.tsx
@@ -3,8 +3,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return
+  return(
   <LoginProvider>
   <Component {...pageProps} />;
   </LoginProvider>
+  );
 }
